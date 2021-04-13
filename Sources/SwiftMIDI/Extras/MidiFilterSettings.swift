91b11a24f2ea46b3ed1b8881e2e3fc2f1d966330
Tristan Leblanc <tristan@moosefactory.eu>
2021-04-13 18:25:39 +0200

    Added iOS version availability for ObservableObject

--- Sources/SwiftMIDI/Extras/MidiFilterSettings.swift
+++ Sources/SwiftMIDI/Extras/MidiFilterSettings.swift
@@ -146,6 +146,7 @@ public class MidiFilterSettings: Codable, Equatable, CustomStringConvertible {
     }
 }
 
+@available(iOS 13.0, *)
 @available(macOS 10.15, *)
 extension MidiFilterSettings: ObservableObject {
     
